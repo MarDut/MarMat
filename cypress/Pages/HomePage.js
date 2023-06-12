f6c01dc955b8efa38916007cb7cab8b4cb7f3749
MarekD <mrdutkiewicz@gmail.com>
2023-06-12 23:29:23 +0200

    poprawka do POP - teraz jest zgodnie ze sztuką

--- cypress/Pages/HomePage.js
+++ cypress/Pages/HomePage.js
@@ -1,28 +1,51 @@
 class HomePage {
 
+// getters
+
+  get productListElement() {
+    return cy.get('.products.content-wrap li');
+  }
+
+  get searchBoxIcon() {
+    return cy.get('.search-toggle-open');
+  }
+
+  get searchBoxInput() {
+    return cy.get('input[class="search-field"]');
+  }
+
+  get menuListElement() {
+     return cy.get('#primary-menu > li > a');
+  }
+
+  get addToCartButton () {
+    return cy.get('.add_to_cart_button');
+  }
+
+
+// methods
+
   visit() {
     return cy.visit('/');
   }
 
-  selectFromMenu(menu){
-    cy.get('#primary-menu > li > a').contains(menu).click()
+  selectFromMenu(menu) {
+    this.menuListElement.contains(menu).click()
   }
 
-  search(searchText){
-    cy.get('.search-toggle-open').click()
-    cy.get('input[class="search-field"]').type(searchText + '{enter}')
+  search(searchText) {
+    this.searchBoxIcon.click()
+    this.searchBoxInput.type(searchText + '{enter}')
   }
 
-  viewAllProducts(){
+  viewAllProducts() {}
 
+  selectProduct(index) {
+    this.productListElement.eq(index).click()
   }
 
-  selectProduct(index){
-    cy.get('.products.content-wrap li').eq(index).click()
-  }
-
-  addToCart(index){
-    cy.get('.add_to_cart_button').eq(index).click()
+  addToCart(index) {
+    this.addToCartButton.eq(index).click()
   }
 }
 
